70a26563d11ccb3c63bfaafad11d9171ade37a73
artem0679 <55602618+artem0679@users.noreply.github.com>
2024-03-27 23:28:33 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ params = {
     "username": "root",
     "password": "password",
     "db_name": "academic_performance",
-    "db_url": "localhost",
+    "db_url": "192.168.2.2",
 }
 
 SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{params.get("username")}:{params.get("password")}@{params.get("db_url")}/{params.get("db_name")}'
